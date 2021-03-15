cfa883b525a684863abe92b37134cd2a7877b8f5
itzAamir <amirpc190320@gmail.com>
2021-03-15 11:50:17 +0530

    added production mode

--- server/server.js
+++ server/server.js
@@ -3,11 +3,11 @@ const app = express();
 const path = require("path");
 const port = process.env.PORT || 5000;
 
-// if (process.env.NODE_ENV === "production") {
-app.use(express.static(path.join(__dirname, "../", "my-app", "build")));
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../", "my-app", "build")));
 
-app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "my-app", "build", "index.html"));
-});
-// }
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "my-app", "build", "index.html"));
+    });
+}
 app.listen(port, () => console.log(`Server started at PORT: ${port}`))
